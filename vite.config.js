84d0eaf3c98021a1d4d9624a09376236daf90727
LuoPeijun <peijun.luo@thoughtworks.com>
2024-04-26 17:48:09 +0800

    chore: import element ui and adjust server port

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,16 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue()
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 8080,
+    open: true
   }
 })
